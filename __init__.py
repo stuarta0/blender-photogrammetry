fd22430df6c4ad5993f4233ccddf2ff7cef7c8e5
Stuart Attenborrow <stuarta0@gmail.com>
2018-05-03 22:39:41 +1000

    Added minimum bounds to PMVS options

--- __init__.py
+++ __init__.py
@@ -104,6 +104,11 @@ class ExportMovieClipBundler(bpy.types.Operator, ExportHelper):
     convert_pmvs = BoolProperty(name='Convert to PMVS', update=convert_updated, description='Convert bundle.out to PMVS format in subdirectory "pmvs"', default=False)
     exec_pmvs = BoolProperty(name='Execute PMVS', update=execute_updated, description='Run PMVS with default settings for dense reconstruction', default=False)
 
+    pmvs_level = IntProperty(name='Level', default=1, min=0, description='When level is 0, original (full) resolution images are used. When level is 1, images are halved (or 4 times less pixels). And so on')
+    pmvs_csize = IntProperty(name='Cell Size', default=2, min=1, description='Controls the density of reconstructions. increasing the value of cell size leads to sparser reconstructions')
+    pmvs_threshold = FloatProperty(name='Threshold', default=0.7, min=0.15, description='A patch reconstruction is accepted as a success and kept, if its associcated photometric consistency measure is above this threshold. The software repeats three iterations of the reconstruction pipeline, and this threshold is relaxed (decreased) by 0.05 at the end of each iteration')
+    pmvs_wsize = IntProperty(name='Window Size', default=7, min=1, description='The software samples wsize x wsize pixel colors from each image to compute photometric consistency score.  Increasing the value leads to more stable reconstructions, but the program becomes slower')
+    pmvs_minImageNum = IntProperty(name='Min Image Num', default=3, min=2, description='Each 3D point must be visible in at least this many images to be reconstructed. If images are poor quality, increase this value')
     #CPU 8
     #setEdge 0
     #useBound 0
